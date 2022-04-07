8de86ecbde7e5178a9ff289bb4929e29ab05d8d3
MalinKirjonen <91899355+MalinKirjonen@users.noreply.github.com>
2022-04-07 13:51:30 +0200

    Created the structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,45 @@
 import React from "react";
-import "./Weather";
+import "./Weather.css";
 
 export default function () {
-  return <div className="Weather">Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather-icon"
+          />{" "}
+          6 °C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
