4d637fc45b6a278694e1e7be0ea9f928052c67a5
MalinKirjonen <91899355+MalinKirjonen@users.noreply.github.com>
2022-04-07 13:14:45 +0200

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        Project coded by{" "}
-        <a
-          href="https://affectionate-swartz-08ee25.netlify.app/"
-          target="_blank"
-          title="visit Malin´s coding portfolio"
-        >
-          Malin Kirjonen
-        </a>{" "}
-        and this is{" "}
-        <a
-          href="https://github.com/MalinKirjonen/react-weather-app"
-          title="open-source link"
-          target="_blank"
-        >
-          Open Sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <button className="btn btn-primary">test</button>
+        <footer>
+          Project coded by{" "}
+          <a
+            href="https://affectionate-swartz-08ee25.netlify.app/"
+            target="_blank"
+            title="visit Malin´s coding portfolio"
+          >
+            Malin Kirjonen
+          </a>{" "}
+          and this is{" "}
+          <a
+            href="https://github.com/MalinKirjonen/react-weather-app"
+            title="open-source link"
+            target="_blank"
+          >
+            Open Sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
