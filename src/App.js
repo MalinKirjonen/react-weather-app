a392e6f9cdb1d218d54d839cc617e7bc62f745c0
MalinKirjonen <91899355+MalinKirjonen@users.noreply.github.com>
2022-04-07 12:23:19 +0200

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        Project coded by{" "}
+        <a
+          href="https://affectionate-swartz-08ee25.netlify.app/"
+          target="_blank"
+          title="visit Malin´s coding portfolio"
+        >
+          Malin Kirjonen
+        </a>{" "}
+        and this is{" "}
+        <a
+          href="https://github.com/MalinKirjonen/react-weather-app"
+          title="open-source link"
+          target="_blank"
+        >
+          Open Sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
